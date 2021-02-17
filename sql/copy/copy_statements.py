e693e1f2e59241dd481987080e3ff070d536e600
Joseph Corrado <josephcorrado7@gmail.com>
2021-02-17 16:49:50 -0500

    separated the load statements so they are more modular and in separate SQL queries

--- sql/copy/copy_statements.py
+++ sql/copy/copy_statements.py
@@ -1,69 +1,79 @@
 """This module contains the COPY commands to load the raw data from the S3
 external stage into staging data tables.
+
+// weather stage contents
+list @UDACITY_WEATHER_S3_STAGE;
+// yelp stage contents
+list @UDACITY_YELP_S3_STAGE;
 """
 
-
-COPY_WEATHER_DATA_FROM_S3 = """
-//print the files found in the weather s3 bucket
-list @UDACITY_WEATHER_S3_STAGE;
-
-//copy the temperature data from the file into the table
+LOAD_TEMPERATURE = """
 COPY INTO NY_TEMPERATURE
   FROM @udacity_weather_s3_stage/USW00094728-temperature-degreeF.csv
   FILE_FORMAT = (format_name = "UDACITYPROJECT"."STAGING".WEATHER_CSV_FORMAT)
   FORCE = TRUE;
+"""
 
-//copy the precipitation data from the file into the table
+LOAD_PRECIPITATION = """
 COPY INTO NY_PRECIPITATION
   FROM @udacity_weather_s3_stage/USW00094728-NY_CITY_CENTRAL_PARK-precipitation-inch.csv
   FILE_FORMAT = (format_name = "UDACITYPROJECT"."STAGING".WEATHER_CSV_FORMAT)
   FORCE = TRUE;
-
-SELECT * FROM NY_TEMPERATURE ORDER BY DATE LIMIT 4;
 """
 
-COPY_YELP_DATA_FROM_S3 = """
-//copy the YELP COVID data from the file into the table
+LOAD_COVID_FEATURES = """
 COPY INTO YELP_COVID_FEATURES
   FROM @udacity_yelp_s3_stage/covid_19_dataset_2020_06_10/yelp_academic_dataset_covid_features.json
   FILE_FORMAT = (format_name = "UDACITYPROJECT"."STAGING".YELP_JSON_FORMAT)
   FORCE = TRUE;
+"""
 
-
-// copy YELP business data from file into table
+LOAD_BUSINESS = """
 COPY INTO YELP_BUSINESS
   FROM @udacity_yelp_s3_stage/yelp_dataset/yelp_academic_dataset_business.json
   FILE_FORMAT = (format_name = "UDACITYPROJECT"."STAGING".YELP_JSON_FORMAT)
   FORCE = TRUE;
-  
+"""
 
-// copy YELP checkin data from file into table
+LOAD_CHECKIN = """
 COPY INTO YELP_CHECKIN
   FROM @udacity_yelp_s3_stage/yelp_dataset/yelp_academic_dataset_checkin.json
   FILE_FORMAT = (format_name = "UDACITYPROJECT"."STAGING".YELP_JSON_FORMAT)
   FORCE = TRUE;
-  
-// copy YELP tip data from file into table
+"""
+
+LOAD_TIP = """
 COPY INTO YELP_TIP
   FROM @udacity_yelp_s3_stage/yelp_dataset/yelp_academic_dataset_tip.json
   FILE_FORMAT = (format_name = "UDACITYPROJECT"."STAGING".YELP_JSON_FORMAT)
   FORCE = TRUE;
-  
-// copy YELP user data from file into table
+"""
+
+LOAD_USERS = """
 COPY INTO YELP_USERS
   FROM @udacity_yelp_s3_stage/yelp_dataset/yelp_academic_dataset_user.json
   FILE_FORMAT = (format_name = "UDACITYPROJECT"."STAGING".YELP_JSON_FORMAT)
   FORCE = TRUE;
-  
-// copy YELP reviews data from file into table
+"""
+
+LOAD_REVIEWS = """
 COPY INTO YELP_REVIEWS
   FROM @udacity_yelp_s3_stage/yelp_dataset/yelp_academic_dataset_review.json
   FILE_FORMAT = (format_name = "UDACITYPROJECT"."STAGING".YELP_JSON_FORMAT)
   FORCE = TRUE;
-  
-// STAGE CONTENTS
-list @UDACITY_YELP_S3_STAGE;
-
-// CHECK TABLE
-select * from YELP_USERS LIMIT 2;
 """
+
+
+COPY_WEATHER_DATA_FROM_S3 = [
+        LOAD_TEMPERATURE,
+        LOAD_PRECIPITATION
+]
+
+COPY_YELP_DATA_FROM_S3 = [
+        LOAD_COVID_FEATURES,
+        LOAD_BUSINESS,
+        LOAD_CHECKIN,
+        LOAD_TIP,
+        LOAD_USERS,
+        LOAD_REVIEWS
+]
